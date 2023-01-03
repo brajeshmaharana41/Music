f97f793c0e54eafbb88a46f81bb7a6886ad5ac67
asitjavarnd <asitking1@gmail.com>
2023-01-03 11:08:49 -0500

    sidebar moodlist dropdown done dynamically with api response and view all open with that mood songs

--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -1,68 +1,54 @@
 import { Component, OnInit } from '@angular/core';
-import {MatTreeNestedDataSource} from '@angular/material/tree';
-import {NestedTreeControl} from '@angular/cdk/tree';
+import {
+  CollectionViewer,
+  SelectionChange,
+  DataSource,
+} from '@angular/cdk/collections';
+import { FlatTreeControl } from '@angular/cdk/tree';
+import { SidebarService } from '../../services/sidebar.service';
+import { BehaviorSubject } from 'rxjs';
+import { DynamicDataSource } from '../../common/dynamicDataSource';
+import * as Type from '../../type/main.type';
+import { CommonService } from '../../services/common.service';
+import { Router } from '@angular/router';
 
-interface FoodNode {
-  icons: string;
-  name: string;
-  link: string;
-  children?: FoodNode[];
+export class DynamicFlatNode {
+  constructor(
+    public item: string,
+    public level = 1,
+    public expandable = false,
+    public isLoading = false
+  ) {}
 }
-
-const TREE_DATA: FoodNode[] = [
-  {
-    icons: 'library_music',
-    name: 'Mood List',
-    link: '',
-    children: [{icons: 'home', name: 'Transactions', link: 'transactions'}, {icons: 'account_circle', name: 'Party', link: 'party'}],
-  },
-  // {
-  //   icons: 'add_shopping_cart',
-  //   name: 'Purchase',
-  //   link: '',
-  //   children: [{icons: '', name: 'Purchase Entry', link: 'purchaseentry'}, {icons: '', name: 'Cash Out', link: 'paymentout'}],
-  // },
-  // {
-  //   icons: 'call_split',
-  //   name: 'Sale',
-  //   link: '',
-  //   children: [{icons: '', name: 'Sale Invoice', link: 'purchaseentry'  }, {icons: '', name: 'Cash In', link: 'paymentin'  }],
-  // },
-  // {
-  //   icons: 'import_contacts',
-  //   name: 'OutStanding',
-  //   link: '',
-  //   children: [{icons: '', name: 'Payable', link: 'payable'  }, {icons: '', name: 'Receivable', link: 'bank'  }],
-  // },
-  // {
-  //   icons: 'arrow_forward',
-  //   name: 'Master Data',
-  //   link: '',
-  //   children: [ {icons: '', name: 'Vehicle', link: 'vehicles'}, 
-  //               {icons: '', name: 'Customer', link: 'customer'}, 
-  //               {icons: '', name: 'Branch', link: 'branch'},
-  //               {icons: '', name: 'Dealers', link: 'dealer'},
-  //               {icons: '', name: 'Party Group', link: 'partygroup'},
-  //               {icons: '', name: 'Finace Company', link: 'financecompany'},
-  //               {icons: '', name: 'Bank', link: 'bank'},
-  //             ],
-  // },
-];
-
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.scss']
+  styleUrls: ['./sidebar.component.scss'],
 })
 export class SidebarComponent implements OnInit {
-  treeControl = new NestedTreeControl<FoodNode>(node => node.children);
-  dataSource = new MatTreeNestedDataSource<FoodNode>();
-  constructor() {
-    this.dataSource.data = TREE_DATA;
+  constructor(
+    public database: SidebarService,
+    public _commonService$: CommonService,
+    public _router$: Router
+  ) {
+    this.treeControl = new FlatTreeControl<DynamicFlatNode>(
+      this.getLevel,
+      this.isExpandable
+    );
+    this.dataSource = new DynamicDataSource(this.treeControl, database);
+
+    this.dataSource.data = database.initialData();
   }
 
-  hasChild = (_: number, node: FoodNode) => !!node.children && node.children.length > 0;
-  
-  ngOnInit(): void {
-  }
+  treeControl: FlatTreeControl<DynamicFlatNode>;
+
+  dataSource: DynamicDataSource;
+
+  getLevel = (node: DynamicFlatNode) => node.level;
+
+  isExpandable = (node: DynamicFlatNode) => node.expandable;
+
+  hasChild = (_: number, _nodeData: DynamicFlatNode) => _nodeData.expandable;
+
+  ngOnInit(): void {}
 }
